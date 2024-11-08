1d742c8dbfd1201cea7ad5254d9bc1af7d49bc22
kun763 <kygoogl1@gmail.com>
2024-11-08 10:52:17 +0800

    L09

--- Game/Levels/Derivative/L09_KunYu.lean
+++ Game/Levels/Derivative/L09_KunYu.lean
@@ -10,42 +10,45 @@ World "Derivative"
 Level 9
 
 Title "The derivative of sin(sin(sin (x)))"
+lemma deriv_sinsinx (x : ℝ) :
+  deriv (fun x => Real.sin ( Real.sin x )) (x : ℝ) = Real.cos x * Real.cos (Real.sin x) := by
 
 
-
-lemma sinsinx_differentiable (x : ℝ) : DifferentiableAt ℝ (fun x => Real.sin ( Real.sin x )) x := by sorry
-
-Statement (x : ℝ) : deriv (fun x => Real.sin ( Real.sin ( Real.sin x ) ) ) (x : ℝ) =
- Real.cos x * Real.cos ( Real.sin x ) * Real.cos ( Real.sin ( Real.sin x ) ) := by
- Hint "This is an application of 'Composite Function Derivative', firstly you need to let lean figure out what is the composite function which means you need to set sin(sinx) as function g"
- Hint "Try tactic: set g := fun x => ..."
- set g := fun x => Real.sin ( Real.sin x )
- Hint "Try to rewrite the question"
- have : (fun x => Real.sin ( Real.sin ( Real.sin x ) )) = Real.sin ∘ g := rfl
- Hint "Try rw[] to apply the assumption"
- rw[this]
- Hint "Now we can use the tactic: deriv.comp"
- rw[deriv.comp]
- Hint "Solve the derivetive by the tactic deriv_.."
- rw[Real.deriv_sin]
- Hint "To solve the derivetive of sin(sinx), we need to have some assumptions by the tactic 'have' and we can prove it in the same way of this question"
- have h : deriv (fun x => Real.sin ( Real.sin x )) (x : ℝ) = Real.cos x * Real.cos (Real.sin x) := by
   set g := fun x => Real.sin x
   have : (fun x => Real.sin (Real.sin x)) = Real.sin ∘ g := rfl
   rw[this]
   rw[deriv.comp]
   rw[Real.deriv_sin]
   rw[mul_comm]
-  Hint "Now we only need to apply Real.differentiableAt_sin"
+
   exact Real.differentiableAt_sin
   exact Real.differentiableAt_sin
- Hint "apply the rewrite tactic"
- rw[h]
- Hint "Try mul_comm"
+
+
+lemma sinsinx_differentiable (x : ℝ) : DifferentiableAt ℝ (fun x => Real.sin ( Real.sin x )) x := by sorry
+
+Statement (x : ℝ) : deriv (fun x => Real.sin ( Real.sin ( Real.sin x ) ) ) (x : ℝ) =
+ Real.cos x * Real.cos ( Real.sin x ) * Real.cos ( Real.sin ( Real.sin x ) ) := by
+ Hint "This is an application of 'Composite Function Derivative', firstly you need to let lean figure out what is the composite function which means you need to set sin(sinx) as function g, which means that we need to turn the goal into : derivative of sin(sin(sin x)) = derivative of sin ∘ g  "
+ Hint "To make the sin(sin x) into the function g, you need to try tactic: set g := fun x => ..."
+ Hint "Remind that sin in lean should be typed as Real.sin"
+ set g := fun x => Real.sin ( Real.sin x )
+ Hint "To prove sin(sin(sin x)) = sin ∘ g, use the have and fun x tactic : have : (fun x => Real.sin (Real.sin (Real.sin x))) = Real.sin ∘ g"
+ Hint "Notice that there is a tap between two sin : Real.sin ( Real.sin ( Real.sin x ) ))"
+ have : (fun x => Real.sin ( Real.sin ( Real.sin x ) )) = Real.sin ∘ g := rfl
+ Hint "Now the goal turn into proving: deriv (Real.sin ∘ g) x = Real.cos x * Real.cos (Real.sin x) * Real.cos (Real.sin (Real.sin x))"
+ Hint "Rewrite the assumption"
+ rw[this]
+ Hint "Now to prove : deriv (Real.sin ∘ g) = deriv (g) * deriv (Real.sin (g)), use the deriv.comp tactic"
+ rw[deriv.comp]
+ Hint "Now the goal are : Proving sin'(x)=cos'(x) and proving g'= cos(x)*cos(sin(x)); Use the Real.deriv_sin tactic"
+ rw[Real.deriv_sin]
+ Hint "To prove g'= cos(x)*cos(sin(x)), the logic of proving is the same and now suppose we already have the tactic 'deriv_sinsinx'"
+ rw[deriv_sinsinx]
+ Hint "The remaining problem is just use commutativity of multiplication"
  rw[mul_comm]
- Hint "Solve the differentiable question by tactic: Real.differentiableAt_sin"
+ Hint "Now the left two goal is to prove functions sinx and sin(sinx) are differentiable, and you can use the tactic 'Real.differentiableAt_sin' and 'sinsinx_differentiable x' "
  exact Real.differentiableAt_sin
- Hint "Suppose that we already have the sinsinx_differentiable"
  exact sinsinx_differentiable x
 
 
